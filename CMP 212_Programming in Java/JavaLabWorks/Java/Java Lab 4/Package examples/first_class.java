c52b6546f4fea0137888ea7af45d7528e08bc5ed
Pradip Dhungana <100480407+dhunganaPradeep@users.noreply.github.com>
2023-02-14 21:57:38 +0545

    Delete CMP 212_Programming in Java/JavaLabWorks directory
    This directory was deleted because all of the code contained within this directory was already in the JAVA directory including lab reports too. Please have a look at that.

--- CMP 212_Programming in Java/JavaLabWorks/Java/Java Lab 4/Package examples/first_class.java
+++
@@ -1,8 +0,0 @@
-package examination;
-public class first_class{
-public void method(){
-System.out.println("First package ko method call bhayo hai !!! ");
-}
- 
-}
-
--- CMP 212_Programming in Java/JavaLabWorks/Java/Unboxing.java
+++
@@ -1,8 +0,0 @@
-class Unboxing {
-    public static void main(String[] args) {
-        Integer i = new Integer(12);
-        int a = i.intValue();
-        int k = i;
-        System.out.println(i + " " + a + " " + k);
-    }
-}
